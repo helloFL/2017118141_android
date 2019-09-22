5f167833f24dddf72467c49c604b7280a05ce48a
HSZ <1105311969@qq.com>
2019-09-23 00:18:06 +0800

    no message

--- helloword/app/src/main/java/com/example/helloword/SecondActivity.java
+++ helloword/app/src/main/java/com/example/helloword/SecondActivity.java
@@ -7,18 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button button1=findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_second);
+        Button button2=findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent1=new Intent(MainActivity.this,SecondActivity.class);
-                startActivity(intent1);
+                Intent intent2=new Intent(SecondActivity.this,MainActivity.class);
+                startActivity(intent2);
             }
         });
     }
