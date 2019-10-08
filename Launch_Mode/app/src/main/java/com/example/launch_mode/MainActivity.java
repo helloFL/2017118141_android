fd0cabbd43f2f8ecc324a2a64f466834ed486600
HSZ <1105311969@qq.com>
2019-10-08 12:41:20 +0800

    默认standard启动模式，可以通过修改activity中的launchMode来修改活动的启动模式

--- Launch_Mode/app/src/main/java/com/example/launch_mode/MainActivity.java
+++ Launch_Mode/app/src/main/java/com/example/launch_mode/MainActivity.java
@@ -6,9 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class FirstActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent1=new Intent(MainActivity.this,SecondActivity.class);
+                Intent intent1=new Intent(FirstActivity.this,SecondActivity.class);
                 startActivity(intent1);
             }
         });
