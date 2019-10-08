dfd0bdac1e9b04c532f7efe95525ff2620507ff3
HSZ <1105311969@qq.com>
2019-10-08 13:27:13 +0800

    no message

--- Launch_Mode/app/src/main/java/com/example/launch_mode/SecondActivity.java
+++ Launch_Mode/app/src/main/java/com/example/launch_mode/SecondActivity.java
@@ -2,7 +2,10 @@ package com.example.launch_mode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Button button2=findViewById(R.id.button_2);
+        Button button3=findViewById(R.id.button_3);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2=new Intent(SecondActivity.this,SecondActivity.class);
+                startActivity(intent2);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent3=new Intent(SecondActivity.this,ThirdActivity.class);
+                startActivity(intent3);
+            }
+        });
     }
 }
--- Launch_Mode/app/src/main/java/com/example/launch_mode/ThirdActivity.java
+++ Launch_Mode/app/src/main/java/com/example/launch_mode/ThirdActivity.java
@@ -2,7 +2,10 @@ package com.example.launch_mode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        Button button3=findViewById(R.id.button_3);
+        Button button4=findViewById(R.id.button_4);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent3=new Intent(ThirdActivity.this,ThirdActivity.class);
+                startActivity(intent3);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent4=new Intent(ThirdActivity.this,FourthActivity.class);
+                startActivity(intent4);
+            }
+        });
     }
 }
